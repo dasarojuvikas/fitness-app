bbcf822ea5acb1bbc5a99807a324be7b6d7f6b1b
Your Name <vtu15504@veltech.edu.in>
2026-07-15 20:08:21 +0530

    name

--- userservice/src/main/java/com/fitness/userservice/service/UserService.java
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+//pavani
 @Service
 @Slf4j
 public class UserService {
